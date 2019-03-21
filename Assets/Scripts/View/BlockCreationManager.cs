0d0dc26c410a25fadc2ff7f5a6d5adb85723045a
Dhruv Jain <dj336@cornell.edu>
2019-03-21 13:55:17 -0400

    Cloned commit

---
+++ Assets/Scripts/View/BlockCreationManager.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+namespace CornellTech.View
+{
+	/// <summary>
+	/// Manages the creation of blocks.
+	/// </summary>
+	public class BlockCreationManager : MonoBehaviour
+	{
+		//Serialized
+		[SerializeField]
+		protected Button dropBlocksButton;
+		
+		/////Protected/////
+		//References
+		protected BlockCreator[] blockCreators;
+		protected List<Block> activeBlocks = new List<Block>();
+		protected List<Block> droppedBlocks = new List<Block>();
+
+		///////////////////////////////////////////////////////////////////////////
+		//
+		// Inherited from MonoBehaviour
+		//
+		
+		protected void Awake ()
+		{
+			blockCreators = GetComponentsInChildren<BlockCreator> ();
+
+			RegisterEvents ();
+		}
+		
+		protected void Start ()
+		{	
+
+		}
+		
+		protected void Update ()
+		{	
+
+		}
+		
+		///////////////////////////////////////////////////////////////////////////
+		//
+		// BlockCreationManager Functions
+		//
+
+		/// <summary>
+		/// Registers the necessary actions/funcs.
+		/// </summary>
+		protected void RegisterEvents()
+		{
+			for (int i = 0; i < blockCreators.Length; i++)
+			{
+				blockCreators [i].BlockCreatedAction += OnBlockCreated;
+				blockCreators [i].GetBlocksFunc += GetBlocks;
+			}
+
+			dropBlocksButton.onClick.AddListener (OnDropBlocksButtonPressed);
+		}
+
+		/// <summary>
+		/// Returns all the active blocks for the creators to check against.
+		/// </summary>
+		/// <returns>The blocks.</returns>
+		protected Block[] GetBlocks()
+		{
+			return this.activeBlocks.ToArray ();
+		}
+		
+		////////////////////////////////////////
+		//
+		// Event Functions
+
+		/// <summary>
+		/// After the drop blocks button is pressed, we add physics and interactivity to the active blocks and update our lists.
+		/// </summary>
+		protected void OnDropBlocksButtonPressed()
+		{
+			//TODO: Fill.
+		}
+
+		/// <summary>
+		/// After the delete blocks is pressed we delete the block gameobjects and update our lists.
+		/// </summary>
+		protected void OnDeleteBlocksButtonPressed()
+		{
+			//TODO: Fill.
+		}
+
+		/// <summary>
+		/// When a block is created we need to add it to our list.
+		/// </summary>
+		/// <param name="block">Block.</param>
+		protected void OnBlockCreated(Block block)
+		{
+			activeBlocks.Add (block);
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,27 @@
+### INFO 5340 / CS 5650
+### Virtual and Augmented Reality 
+# Assignment 4
+
+Read: [Assignment Instructions](https://docs.google.com/document/d/1_BbLDLHxFATOVnj0KIuibR9urb0eygCADz5p2WJRJ-k/edit?ts=5bd23973 "Detailed Assignment Instructions")
+
+<hr>
+
+### Student Name:
+
+[Insert your full name]
+
+### Student Email:
+
+[Insert your Cornell email address]
+
+### Solution (Screen Recording):
+
+[Insert YouTube, Vimeo etc. link for a screen recording of your solution in action]
+
+### Work Summary:
+
+[Write a short summary of your approach and list challenges]
+
+### Super Mode Implementation:
+
+[Explain your Super Mode implementation in detail. Remember to also display it clearly in your Solution Video above]
